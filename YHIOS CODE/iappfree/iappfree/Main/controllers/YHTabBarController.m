1b2c2b4e1db8f539ce4a6374b3d69ccebfc1695f
YH <812446905@qq.com>
2016-10-03 17:01:31 +0800

    Update code

--- YHIOS CODE/iappfree/iappfree/Main/controllers/YHTabBarController.m
+++ YHIOS CODE/iappfree/iappfree/Main/controllers/YHTabBarController.m
@@ -17,6 +17,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    for (UIBarItem *item in self.tabBar.items) {
+        [item setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
+                                      [UIFont fontWithName:@"Helvetica" size:13.0], NSFontAttributeName, nil]
+                            forState:UIControlStateNormal];
+        
+    }
+
+    
 }
 
 - (void)didReceiveMemoryWarning {
